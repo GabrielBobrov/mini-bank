bcc4f34ee377c0f7186984a09226eec64930ca09
Gabriel Bobrov <bielbobrov@hotmail.com>
2023-08-23 21:26:51 -0300

    first commit

---
+++ ms-transfer/ms-transfers/src/test/java/com/minibank/transfers/TransfersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minibank.transfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
